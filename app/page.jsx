7c4aa933d64d86f418afb092b28644f191dd9dbd
Ani <turboji007@gmail.com>
2025-01-03 15:14:54 +0530

    testing deployment

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,44 @@
-import Hero from "../components/ui/Hero.jsx";
-import { NewContact } from "@/components/ui/Contact.jsx";
-import Highlight from "@/components/ui/Blogs/Highlight.jsx";
-import TechStack from "@/components/ui/Tech/Techstack.jsx";
-import Projects from "@/components/project/Projects.jsx";
+import TechStack from "@/components/ui/Tech/Techstack.js";
+import Timeline from "@/components/ui/timeline.jsx"; // Corrected import path
 
-
-export default function Home() {
+const Page = () => {
   return (
-
-    <div
-    id="app"
-    className="flex dark flex-col md:items-start text-white w-full"
-    >
- 
-      <Hero />
-      <Projects />
+    <div>
       <TechStack />
-      <Highlight />
-      <NewContact />
+      <Timeline /> {/* Add the Timeline component if needed */}
     </div>
-
-  )
+  );
 }
+
+export default Page;
+
+
+
+
+
+
+
+// import Hero from "../components/ui/Hero.jsx";
+// import { NewContact } from "@/components/ui/Contact.jsx";
+// import Highlight from "@/components/ui/Blogs/Highlight.jsx";
+// import TechStack from "@/components/ui/Tech/Techstack.jsx";
+// import Projects from "@/components/project/Projects.jsx";
+
+
+// export default function Home() {
+//   return (
+
+//     <div
+//     id="app"
+//     className="flex dark flex-col md:items-start text-white w-full"
+//     >
+ 
+//       <Hero />
+//       <Projects />
+//       <TechStack />
+//       <Highlight />
+//       <NewContact />
+//     </div>
+
+//   )
+// }
