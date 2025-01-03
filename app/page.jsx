228008e3b7de6e0f96a5b0626a3fad9333e74476
Ani <turboji007@gmail.com>
2025-01-03 14:48:58 +0530

    testing deployment

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Hero from "../components/ui/Hero.jsx";
 import { NewContact } from "@/components/ui/Contact.jsx";
 import Highlight from "@/components/ui/Blogs/Highlight.jsx";
 import TechStack from "@/components/ui/Tech/Techstack.jsx";
-import Timeline from "@/components/ui/Timeline.jsx";
+// import Timeline from "@/components/ui/Timeline.jsx";
 import Projects from "@/components/project/Projects.jsx";
 
 
@@ -17,7 +17,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <TechStack />
-      <Timeline  />
+      {/* <Timeline  /> */}
       <Highlight />
       <NewContact />
     </div>
