0674f239ae65f733558d1a3427649d99fee9abf1
Ani <turboji007@gmail.com>
2025-02-12 15:25:30 +0530

    changed text in  blog components and update hero section

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,64 +1,3 @@
-// import { Blog } from "@/components/ui/Blogs/Blog";
-// import { fetchAllLivePages, fetchPages } from "@/lib/notion";
-// // import Link from "next/link";
-
-// export default async function page() {
-//   const response = await fetchPages();
-//   const posts = response.results;
-
-//   // console.log("this is post", posts[0]);
-//   console.log("target", posts[0].properties.slug.rich_text[0].text.content );
-
-//   // const title = posts[0].properties.Title.rich_text[0].text.content | "Untitled";
-//   // const Date = posts[0].properties.Date.created_time;
-//   // const tags = posts[0].properties.Tags.multi_select; // its and array of tags
-//   // const subHead = posts[0].properties.subhead.rich_text[0].text.content;
-
-//   // console.log(posts[0].properties.Title.title[0].plain_text);
-//   // console.log(posts[0].properties.Tags.multi_select);
-//   // console.log(posts[0].properties.subhead.rich_text[0].text.content);
-//   // console.log(posts[0].properties.slug.rich_text[0].text.content);
-
-  
-
-//   return (
-//     <div className="cards flex flex-col gap-4 md:mt-6 ">
-//       <header className="bg-background p-4 rounded-md shadow-md  md:mb-6">
-//         <h1 className="text-2xl font-bold mb-2 md:text-4xl py-6">
-//           Hello World
-//         </h1>
-//         <div className="text-gray-300">
-//           Welcome to my blog! I'm a passionate developer and tech enthusiast,
-//           diving deep into web development, system design, and problem-solving
-//           with DSA. When I'm not coding, I'm likely exploring innovative ideas
-//           for projects, mentoring platforms, or working on creative solutions
-//           for clients. I enjoy tackling challenges, whether it's creating
-//           efficient algorithms, building robust web apps with the MERN stack, or
-//           writing compelling blog posts. Stay tuned for insights, tips, and
-//           stories from my journey in tech!
-//         </div>
-//       </header>
-
-//       {posts && posts.map((post) => {
-//        const date = new Date(post.properties.Date.created_time).toLocaleDateString("en-GB")
-
-//         return (
-//           <Blog
-//             title={post.properties.Title.rich_text[0].text.content}
-//             date={date}
-//             tags={post.properties.Tags.multi_select} // its an array
-//             slug={post.properties.slug.rich_text[0].text.content}
-//             subHead={post.properties.subhead.rich_text[0].text.content}
-//           />
-//         );
-//       })}
-//       { !posts && <div>No live blogs available</div> }
-//     </div>
-//   );
-// }
-
-
-
 import { Blog } from "@/components/ui/Blogs/Blog";
 import { fetchPages } from "@/lib/notion";
 
@@ -76,14 +15,11 @@ export default async function Page() {
           Hello World
         </h1>
         <div className="text-gray-300">
-          Welcome to my blog! I'm a passionate developer and tech enthusiast,
-          diving deep into web development, system design, and problem-solving
-          with DSA. When I'm not coding, I'm likely exploring innovative ideas
-          for projects, mentoring platforms, or working on creative solutions
-          for clients. I enjoy tackling challenges, whether it's creating
-          efficient algorithms, building robust web apps with the MERN stack, or
-          writing compelling blog posts. Stay tuned for insights, tips, and
-          stories from my journey in tech!
+        Welcome to my blog! I'm a Student, who loves to build thing with code, 
+          and i also like to explore new thechnology,
+          <div className="w-full h-5"></div>
+          and as i am exploring someting i like to write about it. and it just not about technical writing i like to write as usual, 
+          and i also want to improve on it further, therefor here this is my blog...
         </div>
       </header>
 
--- components/ui/Blogs/Post.jsx
+++ components/ui/Blogs/Post.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-
-// export default function Post({ title, date, htmlContent }) {
-//   return (
-//     <div className="w-full max-w-4xl mx-auto py-6 ">
-//       <h1 className="text-3xl italic">{title || "Untitled"}</h1>
-//       <p className="text-sm text-gray-500 mb-2">
-//         {date}
-//       </p>
-//       <hr />
-//       <div
-//         className="prose mt-10 max-w-none text-primary dark:prose-invert 
-//                    prose-img:rounded-xl prose-img:mx-auto"
-//         dangerouslySetInnerHTML={{ __html: htmlContent }}
-//       />
-//     </div>
-//       // <div className="prose mt-10  dark:prose-headings:text-white max-w-none" dangerouslySetInnerHTML={{ __html: htmlContent }} />
-//     // </div>
-//   );
-// }
-
 "use client"
 
 import React, { useEffect } from "react";
